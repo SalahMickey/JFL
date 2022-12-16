6235841926fdf38f02ddb87d7ea33b9d92b80912
SalahMicky <salahmicky@gmail.com>
2022-12-16 21:29:35 +0300

    Added Comments to code Customization

--- odoo-custom-addons/customizations/models/models.py
+++ odoo-custom-addons/customizations/models/models.py
@@ -1,22 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-# class customizations(models.Model):
-#     _name = 'customizations.customizations'
-#     _description = 'customizations.customizations'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+#Model
 class hrEmployeeInherit(models.Model):
-    _inherit = "res.users"
+    _inherit = "res.users" # Inherited Model
 
     additional_note = fields.Char(string='Additional Note')
 
