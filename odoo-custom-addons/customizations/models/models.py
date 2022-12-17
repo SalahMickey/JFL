8c52d40ef887084b466eea56a6edfa5efb3c8353
SalahMicky <salahmicky@gmail.com>
2022-12-17 19:29:16 +0300

    Added Bank info to user

--- odoo-custom-addons/customizations/models/models.py
+++ odoo-custom-addons/customizations/models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 #Model
 class hrEmployeeInherit(models.Model):
-    _inherit = "hr.employees" # Inherited Model
+    _inherit = "res.users" # Inherited Model
 
     bank_code = fields.Char(string='Bank Code')
-    bank_acc_num = fields.Char(string='Bank Account')
+    bank_acc_num = fields.Char(string='Bank account')
