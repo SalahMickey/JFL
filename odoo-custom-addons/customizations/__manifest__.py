ccd8733aaf4a6d956f6c1bf16c869069b37ec277
SalahMicky <salahmicky@gmail.com>
2022-12-17 19:34:49 +0300

    Added Bank info to user

--- odoo-custom-addons/customizations/__manifest__.py
+++ odoo-custom-addons/customizations/__manifest__.py
@@ -24,7 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/hr.inherited.xml',
+        #'views/hr.inherited.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
