6de15e3ab6995ab7fdbc6987fec311977bf47761
Salah <Salahmicky@gmail.com>
2023-01-12 20:56:35 +0300

    Added Penalties

--- odoo-custom-addons/customizations/models/models.py
+++ odoo-custom-addons/customizations/models/models.py
@@ -23,3 +23,9 @@ class hrEmployeeInherit(models.Model):
 
     bank_acc = fields.Char(string='Additional Note')
 
+class Penalties(models.Model):
+    _name = "customizations.penalties"
+    _description = "New Form"
+
+    employee_id = fields.Many2one('hr.employee', string='Employee', required=True, readonly=True, help="Employee",
+                                  states={'draft': [('readonly', False)]})
